b0b5676cb24e1264687c197c462e05e57f29f68c
Ni5h4n <nsquare.37@gmail.com>
2025-05-31 20:31:01 +0530

    Update: Replace deprecated images.domains with remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.unsplash.com'],
-    formats: ['image/avif', 'image/webp'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   experimental: {
     optimizeCss: true,
@@ -22,8 +26,8 @@ const nextConfig = {
         {
           loader: 'file-loader',
           options: {
-            publicPath: '/_next',
-            name: 'static/media/[name].[hash].[ext]',
+            publicPath: '/_next/static/images',
+            outputPath: 'static/images',
           },
         },
       ],
